1df9b003cda6682aef830ec06f291306d4911e55
Jeffrey Perparas <jperparas1@toromail.csudh.edu>
2026-03-02 03:21:01 +0000

    add neighbor_states_query to services, add valid_states.py dataset

--- services/big_query_service.py
+++ services/big_query_service.py
@@ -1,4 +1,5 @@
 from google.cloud import bigquery
+from datasets.valid_states import VALID_STATES
 
 class BigQueryService:
     def __init__(self, project_id ,api_key):
@@ -8,4 +9,20 @@ class BigQueryService:
                 )
         self.df = None
 
+    
+    def query_border_states(self,state_name):
+         if state_name.lower() not in VALID_STATES:
+             raise ValueError(f"Invalid state: {state_name}")
+         state_name = state_name.title()
+         sql = """
+            SELECT neighbors_state
+            FROM `bigquery-public-data.geo_us_boundaries.adjacent_states`
+            WHERE  state_name = @state_name
+        """
+         job_config = bigquery.QueryJobConfig(
+                 query_parameters=[
+                            bigquery.ScalarQueryParameter("state_name" , "STRING", state_name) 
+                     ]
+                 )
+         self.df =  self.client.query(sql,job_config = job_config).to_dataframe()
 
